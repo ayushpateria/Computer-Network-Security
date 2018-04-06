ef002219987a8e722d642a0be746d78d0c93417d
sipian <cs15btech11019@iith.ac.in>
2018-04-06 18:52:47 +0530

    add functions for ip prefixing

--- Assignments/Assgn-4-Firewall/firewall.py
+++ Assignments/Assgn-4-Firewall/firewall.py
@@ -1,18 +1,12 @@
 #Program to build a mini firewall using python
 
-import struct
 import sys
 import time
+import math
+import struct
 import socket
 from netfilterqueue import NetfilterQueue
-from socket import AF_INET, AF_INET6, inet_ntoa
-import queue
-from scapy.all import *
-
-#from main import PKT_DIR_INCOMING, PKT_DIR_OUTGOING
-from math import ceil
-import re
-import binascii
+from socket import inet_ntoa
 
 global action, inputprotocol, actiontype, inputportnum, inputipaddr
 
@@ -22,177 +16,205 @@ class Firewall:
         self.srcipaddress = ''
         self.srcportnum = 0
 
-    #""" Checks the packet to make sure the IPv4 address is valid.
-        #if valid it returns true else false"""
-    def valid_IP_address(self, ext_addr):
-        try:
-           socket.inet_ntoa(ext_addr)
-           return True
-        except socket.error:
-           return False
-
-    def obtain_fields(self, pckt):
-        try:
-            protocol = struct.unpack('!B', pckt[9:10]) # (integer,)
-            total_length = struct.unpack('!H', pckt[2:4])
-            return self.strip_format(protocol), self.strip_format(total_length)
-        except struct.error as e:
-            print(e)
-            return None, None
-
-    def valid_ip_header(self, pckt):
-        try:
-            #print pckt
-            ip_header = struct.unpack('!B', pckt[0:1])
-            return self.strip_format(ip_header)
-        except struct.error as e:
-            print(e)
-            print(pckt[0:1])
-            return None
-
-    def get_udp_length(self, pckt, startIndex):
-        try:
-            length = struct.unpack('!H', pckt[startIndex + 4 : startIndex + 6])
-            return self.strip_format(length)
-        except struct.error:
-            return None
-
-        #""" Protocol Selector."""
-
-    def protocol_selector(self, protocol):
+    # Since struct unpack gives a tuple as result, this function removes unnecessary characters (,) and returns the number
+    def strip_format(self, format_str):
+        new_str = str(format_str)
+        return int(new_str[1: len(new_str) - 2])
+    
+    def get_packet_directionection(self, direction):
+        if (direction == 'outgoing'):
+            return 'outgoing'
+        else:
+            return 'incoming'
+    # functions for protocols
+    def get_protocol(self, protocol):
         if (protocol == 1):
             return "icmp"
         elif (protocol == 6):
             return 'tcp'
         elif (protocol == 17):
             return 'udp'
-        return None
+        else:
+            return None
 
-        #""" Returns True if the protocol of the packet is either TCP, UDP, or ICMP.
-            #Else, the method returns False. """
-
-    def check_protocol(self, protocol):
+    def is_protocol_supported(self, protocol):
         return (protocol == 'tcp') or (protocol == 'udp') or (protocol == 'icmp')
 
-        #""" Returns True if the external IP address is within the range of the
-            #IP prefix."""
-
-    def within_range(self, start_port, end_port, external_ip):
-        return external_ip >= start_port and external_ip <= end_port
-
-        #""" Check if the data is an IP prefix."""
-
-    def is_IP_Prefix(self, data):
-        return data.find('/')
-
-        #""" Strips the parentheses and comma off the number and converts string to int."""
-
-    def strip_format(self, format_str):
-        new_str = str(format_str)
-        return int(new_str[1: len(new_str) - 2])
-
-        #""" Returns the external port and checks to see if there is a socket error. If
-            #the port is valid, then it returns a number, else it returns 'None'. """
-
-    def handle_external_port(self, pckt, startIndex):
+    def get_protocol_packet_length(self, packet):
         try:
-            ext_port = pckt[startIndex: startIndex + 2]
-            ext_port = struct.unpack('!H', ext_port)
-            return ext_port
+            protocol = struct.unpack('!B', packet[9:10])
+            total_length = struct.unpack('!H', packet[2:4])
+            return self.strip_format(protocol), self.strip_format(total_length)
+        except struct.error as e:
+            print(e)
+            return None, None
+
+    # functions for port
+    def get_port(self, packet, startIndex):
+        try:
+            port = packet[startIndex: startIndex + 2]
+            port = struct.unpack('!H', port)
+            return port
         except struct.error:
             return None
 
-        #""" Returns the TYPE field for the IMCP packet."""
+    def is_port_in_range(self, port , start_port, end_port = None):
+        if (end_port):
+            return port >= start_port and port <= end_port
+        else:
+            return port == start_port
 
-    def handle_icmp_packet(self, pckt, startIndex):
+    # functions for IP address
+    def get_ip_header_length(self, packet):
         try:
-            type_field = pckt[startIndex: startIndex + 1]
+            ip_header_length = struct.unpack('!B', packet[0:1])
+            return self.strip_format(ip_header_length)
+        except struct.error as e:
+            print(e)
+            print(packet[0:1])
+            return None
+
+    def is_valid_IP_address(self, ext_addr):
+        try:
+           socket.inet_ntoa(ext_addr)
+           return True
+        except socket.error:
+           return False
+
+    def get_IP_address(self, data, netmask = None):
+        data = ''.join(data.split("."))
+        if(netmask):
+            ip = int(data)
+            bitmask = math.pow(2,32 - netmask) * (math.pow(2,netmask) - 1)
+            return (ip & bitmask) >> (32 - netmask)
+        else
+            return int(data)
+
+    def get_IP_netmask(self, data):
+        if (data.find('/')):
+            return data[0:data.find("/")], int(data[data.find("/")+1])
+        else:
+            return data, None
+
+    def is_valid_IP_range(self, data):
+        if (data.find('/')):
+            netmask = data[data.find("/")+1]
+            try:
+                inetmask = int(netmask)
+                if(inetmask < 0):
+                    print("PACKET ERROR :: Invalid netmask `",netmask,"` specified")
+                    return False
+                return self.is_valid_IP_address(data[0:data.find("/")])
+            except ValueError:
+                print("PACKET ERROR :: Invalid netmask `",netmask,"` specified")
+                return False
+        else:
+            return self.is_valid_IP_address(data)
+
+    def is_IP_in_range(self, ip, allowed_ip):
+        allowed_ip, netmask = self.get_IP_netmask(allowed_ip)
+        if netmask == 0:
+            return True
+        elif(netmask == None):
+            if (ip == allowed_ip):
+                return True
+            else:
+                return False
+        elif(self.get_IP_address(allowed_ip, netmask) == self.get_IP_address(ip, netmask)):
+            return True
+        else:
+            return False
+
+
+    # functions for ICMP   
+    def handle_icmp_packet(self, packet, startIndex):
+        try:
+            type_field = packet[startIndex: startIndex + 1]
             type_field = struct.unpack('!B', type_field)
             return self.strip_format(type_field)
         except struct.error:
             return None
 
-        #""" Returns the direction of the packet in a string."""
+    #functions for UDP
+    def get_udp_length(self, packet, startIndex):
+        try:
+            length = struct.unpack('!H', packet[startIndex + 4 : startIndex + 6])
+            return self.strip_format(length)
+        except struct.error:
+            return None
 
-    def packet_direction(self, direction):
-        if (direction == 'outgoing'):
-            return 'outgoing'
-        else:
-            return 'incoming'
-
-
-            # @pkt_dir: either PKT_DIR_INCOMING or PKT_DIR_OUTGOING
-    # @pkt: the actual data of the IPv4 packet (including IP header)
-    def handle_packet(self, pckt_dir, pckt):
-        ip_header = self.valid_ip_header(pckt)
+    def handle_packet(self, packet_direction, packet):
+        #network packets are big-endian 
+        ip_header = self.get_ip_header_length(packet)
 
         if (ip_header == None):
-            print(1)
-            return
-        ip_header = ip_header & 0x0f
-        if (ip_header < 5):
-            print(2)
-            return
-        print("inside")
+            print("PACKET ERROR :: Invalid ip_header")
+            return False
+
+        ip_header = ip_header & 0x0f    #get last 4 bits - header_length
+        if (ip_header < 5):        #minimum value of correct header length is 5
+            print("PACKET ERROR :: Header length should be >= 5")
+            return False
         
-        protocol, total_length = self.obtain_fields(pckt)
-        if (protocol == None and total_length == None):
-            print(3)
-            return
+        protocol, total_length = self.get_protocol_packet_length(packet)
+        if (protocol == None || total_length == None):
+            print("PACKET ERROR :: Cannot Determine protocol/total_length from packet")
+            return False
 
-        if (total_length != len(pckt)):
-            print(4)
-            return
+        if (total_length != len(packet)):
+            print("PACKET ERROR :: Bytes missing from packet")
+            return False
 
-        if (self.protocol_selector(protocol) == None):
-            #self.send_packet(pckt, pckt_dir)
-            print(5)
-            return
+        if (self.get_protocol(protocol) == None):
+            print("PACKET ERROR :: Protocol not supported")
+            return False
 
-        src_addr, dst_addr, pckt_dir = pckt[12:16], pckt[16:20], self.packet_direction(pckt_dir)
-        if (pckt_dir == 'incoming'):
+        src_addr, dst_addr, packet_direction = packet[12:16], packet[16:20], self.get_packet_directionection(packet_direction)
+        if (packet_direction == 'incoming'):
             external_addr = src_addr
         else:
             external_addr = dst_addr
-        if not (self.valid_IP_address(external_addr)): # check valid address.
+        if not (self.is_valid_IP_address(external_addr)): # check valid address.
             print(6)
             return
 
         if (protocol == 6): # TCP
-            if (pckt_dir == 'incoming'):
-                external_port = self.handle_external_port(pckt, (ip_header) * 4)
+            if (packet_direction == 'incoming'):
+                external_port = self.handle_external_port(packet, (ip_header) * 4)
             else:
-                external_port = self.handle_external_port(pckt, ((ip_header) * 4) + 2)
+                external_port = self.handle_external_port(packet, ((ip_header) * 4) + 2)
             if (external_port == None): # drop packet due to port socket error.
                 print(7)
                 return
 
         elif (protocol == 1): # ICMP
-            type_field = self.handle_icmp_packet(pckt, (ip_header * 4))
+            type_field = self.handle_icmp_packet(packet, (ip_header * 4))
             if (type_field == None):
                 print(8)
                 return
 
         elif (protocol == 17): # UDP
-            udp_length = self.get_udp_length(pckt, (ip_header * 4))
+            udp_length = self.get_udp_length(packet, (ip_header * 4))
             if (udp_length == None or udp_length < 8):
+                #minimum UDP length : 8
                 print(9)
                 return
-            if (pckt_dir == 'incoming'):
-                external_port = self.handle_external_port(pckt, (ip_header) * 4)
+            if (packet_direction == 'incoming'):
+                external_port = self.handle_external_port(packet, (ip_header) * 4)
                 if (external_port == None):
                     print(10)
                     return
 
         verdict = "pass"
-        self.protocolname = self.protocol_selector(protocol)
+        self.protocolname = self.get_protocol(protocol)
         self.srcipaddress = external_addr
         if (protocol != 1):
             self.srcportnum = external_port
 
 
 def cb(payload):
-    print("get packet")
+    print("received packet len :: ", payload.get_payload_len())
+
     data = payload.get_payload()
     f = Firewall()
     f.handle_packet("incoming", data)
