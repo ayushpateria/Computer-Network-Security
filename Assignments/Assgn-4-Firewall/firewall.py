1a3771474f865eba384278ceaa620aa7578e122c
Ayush Pateria <cs15btech11007@iith.ac.in>
2018-04-14 23:46:24 +0530

    Incomplete ipv6

--- Assignments/Assgn-4-Firewall/firewall.py
+++ Assignments/Assgn-4-Firewall/firewall.py
@@ -8,11 +8,23 @@ import psutil
 from IPy import IP
 from colorama import Fore, Style
 from cli import *
+import ipaddr
 
 class Firewall:
     def __init__(self):
         self.scanLog = []
         self.scanLimits = {0: 5, 1: -1, 6: 5, 17: 50} # 0 is for default, -1 is no limit
+
+    def get_version(self, packet):
+        try:
+            ver = (ord(packet[0:1]) & 240) >> 4
+            print(ver)
+            return ver
+        except struct.error:
+            return None    
+
+    ## IPV4
+
 
     # functions for port
     def get_port(self, packet, startIndex):
@@ -23,9 +35,14 @@ class Firewall:
         except struct.error:
             return None         
 
-
-    def is_port_in_range(self, port , start_port, end_port):
-        return port >= start_port and port <= end_port
+    def get_protocol_packet_length(self, packet):
+        try:
+            protocol = struct.unpack('!B', packet[9:10])
+            total_length = struct.unpack('!H', packet[2:4])
+            return self.strip_format(protocol), self.strip_format(total_length)
+        except struct.error as e:
+            print(e)
+            return None, None
 
     # functions for IP address
     def get_ip_header_length(self, packet):
@@ -36,7 +53,41 @@ class Firewall:
             print(e)
             print(packet[0:1])
             return None
-    
+
+    # functions for ICMP   
+    def get_icmp_packet(self, packet, startIndex):
+        try:
+            type_field = packet[startIndex: startIndex + 1]
+            type_field = struct.unpack('!B', type_field)
+            return self.strip_format(type_field)
+        except struct.error:
+            return None
+
+    #functions for UDP
+    def get_udp_length(self, packet, startIndex):
+        try:
+            length = struct.unpack('!H', packet[startIndex + 4 : startIndex + 6])
+            return self.strip_format(length)
+        except struct.error:
+            return None
+
+
+    ## IPV6
+
+    def get_protocol_ipv6(self, packet):
+        try:
+            protocol = struct.unpack('!B', packet[6:7])
+            return self.strip_format(protocol)
+        except struct.error as e:
+            print(e)
+            return None, None
+
+
+
+
+    def is_port_in_range(self, port , start_port, end_port):
+        return port >= start_port and port <= end_port
+
     def get_IP_address(self, data, netmask = None):
         data = ''.join(data.split("."))
         if netmask:
@@ -67,14 +118,7 @@ class Firewall:
         else:
             return None
 
-    def get_protocol_packet_length(self, packet):
-        try:
-            protocol = struct.unpack('!B', packet[9:10])
-            total_length = struct.unpack('!H', packet[2:4])
-            return self.strip_format(protocol), self.strip_format(total_length)
-        except struct.error as e:
-            print(e)
-            return None, None
+
 
     def is_IP_in_range(self, ip, allowed_ip):
         allowed_ip, netmask = self.get_IP_netmask(allowed_ip)
@@ -91,22 +135,7 @@ class Firewall:
             return False
 
 
-    # functions for ICMP   
-    def get_icmp_packet(self, packet, startIndex):
-        try:
-            type_field = packet[startIndex: startIndex + 1]
-            type_field = struct.unpack('!B', type_field)
-            return self.strip_format(type_field)
-        except struct.error:
-            return None
 
-    #functions for UDP
-    def get_udp_length(self, packet, startIndex):
-        try:
-            length = struct.unpack('!H', packet[startIndex + 4 : startIndex + 6])
-            return self.strip_format(length)
-        except struct.error:
-            return None
 
     # check_port_scan works by keeping the record of invalid port requested by each IP in last seconds in memory.
     def check_port_scan(self, srcip, port, prot):
@@ -155,81 +184,129 @@ class Firewall:
 
     def handle_packet(self, packet, rule):
         #network packets are big-endian 
-        ip_header = self.get_ip_header_length(packet)
+        ver = self.get_version(packet)
 
-        if (ip_header == None):
-            print(Fore.YELLOW + "ERROR :: Invalid ip_header" + Style.RESET_ALL)
-            return None
+        if (ver == 4):
+            ip_header = self.get_ip_header_length(packet)
 
-        ip_header = ip_header & 0x0f    #get last 4 bits - header_length
-        if (ip_header < 5):        #minimum value of correct header length is 5
-            print(Fore.YELLOW + "ERROR :: Header length should be >= 5" + Style.RESET_ALL)
-            return None
-        
-        protocol, total_length = self.get_protocol_packet_length(packet)
-        if (protocol == None or total_length == None):
-            print(Fore.YELLOW + "ERROR :: Cannot Determine protocol/total_length from packet" + Style.RESET_ALL)
-            return None
-
-        if (total_length != len(packet)):
-            print(Fore.YELLOW + "ERROR :: Bytes missing from packet" + Style.RESET_ALL)
-            return None
-
-        if (self.get_protocol(protocol) == None):
-            print(Fore.YELLOW + "ERROR :: Protocol not supported" + Style.RESET_ALL)
-            return None
-
-        src_addr, dst_addr = packet[12:16], packet[16:20]
-        if not (is_valid_IP_address(src_addr) and is_valid_IP_address(dst_addr)): # check valid address.
-            print(Fore.YELLOW + "ERROR :: IP addresses are incorrect" + Style.RESET_ALL)
-            return None
-
-        #UDP validation
-        if protocol == 17:
-            udp_length = self.get_udp_length(packet, (ip_header * 4))
-            if (udp_length == None or udp_length < 8):      #minimum UDP length : 8
-                print(Fore.YELLOW + "ERROR :: UDP length should be >= 8" + Style.RESET_ALL)
+            if (ip_header == None):
+                print(Fore.YELLOW + "ERROR :: Invalid ip_header" + Style.RESET_ALL)
                 return None
 
-        #filter protocol
-        if (rule["protocol"] != "all") and (rule["protocol"] != self.get_protocol(protocol)):
-            print(Fore.YELLOW + "FILTER :: Protocol not matched" + Style.RESET_ALL)
-            return False
+            ip_header = ip_header & 0x0f    #get last 4 bits - header_length
+            if (ip_header < 5):        #minimum value of correct header length is 5
+                print(Fore.YELLOW + "ERROR :: Header length should be >= 5" + Style.RESET_ALL)
+                return None
+            
+            protocol, total_length = self.get_protocol_packet_length(packet)
+            if (protocol == None or total_length == None):
+                print(Fore.YELLOW + "ERROR :: Cannot Determine protocol/total_length from packet" + Style.RESET_ALL)
+                return None
 
-        #filter IP addresses
-        if rule["sourceip"] != "any":
-            if not self.is_IP_in_range(socket.inet_ntoa(src_addr), rule["sourceip"]):
-                print(Fore.YELLOW + "FILTER :: IP not in specified range" + Style.RESET_ALL)
+            if (total_length != len(packet)):
+                print(Fore.YELLOW + "ERROR :: Bytes missing from packet" + Style.RESET_ALL)
+                return None
+
+            if (self.get_protocol(protocol) == None):
+                print(Fore.YELLOW + "ERROR :: Protocol not supported" + Style.RESET_ALL)
+                return None
+
+            src_addr, dst_addr = packet[12:16], packet[16:20]
+            if not (is_valid_IP_address(src_addr) and is_valid_IP_address(dst_addr)): # check valid address.
+                print(Fore.YELLOW + "ERROR :: IP addresses are incorrect" + Style.RESET_ALL)
+                return None
+
+            #UDP validation
+            if protocol == 17:
+                udp_length = self.get_udp_length(packet, (ip_header * 4))
+                if (udp_length == None or udp_length < 8):      #minimum UDP length : 8
+                    print(Fore.YELLOW + "ERROR :: UDP length should be >= 8" + Style.RESET_ALL)
+                    return None
+
+            #filter protocol
+            if (rule["protocol"] != "all") and (rule["protocol"] != self.get_protocol(protocol)):
+                print(Fore.YELLOW + "FILTER :: Protocol not matched" + Style.RESET_ALL)
                 return False
-        print("PROTOCOL", protocol)
-        #filter ports
-        if ((protocol == 6) or (protocol == 17)) and ((rule["protocol"] == "all") or (rule["protocol"] == "tcp") or (rule["protocol"] == "udp")):
-            if rule["sport1"]:
-                port = self.get_port(packet, (ip_header) * 4)
-                if port == None:
-                    print(Fore.YELLOW + "ERROR :: Source Port is incorrect" + Style.RESET_ALL)
-                    return None
-                elif not self.is_port_in_range(port, rule["sport1"], rule["sport2"]):
-                    print(Fore.YELLOW + "FILTER :: Source Port is not in specified range" + Style.RESET_ALL)
+
+            #filter IP addresses
+            if rule["sourceip"] != "any":
+                if not self.is_IP_in_range(socket.inet_ntoa(src_addr), rule["sourceip"]):
+                    print(Fore.YELLOW + "FILTER :: IP not in specified range" + Style.RESET_ALL)
                     return False
+            #filter ports
+            if ((protocol == 6) or (protocol == 17)) and ((rule["protocol"] == "all") or (rule["protocol"] == "tcp") or (rule["protocol"] == "udp")):
+                if rule["sport1"]:
+                    port = self.get_port(packet, (ip_header) * 4)
+                    if port == None:
+                        print(Fore.YELLOW + "ERROR :: Source Port is incorrect" + Style.RESET_ALL)
+                        return None
+                    elif not self.is_port_in_range(port, rule["sport1"], rule["sport2"]):
+                        print(Fore.YELLOW + "FILTER :: Source Port is not in specified range" + Style.RESET_ALL)
+                        return False
 
-            if rule["dport1"]:
-                port = self.get_port(packet, ((ip_header) * 4) + 2)
-                if port == None:
-                    print(Fore.YELLOW + "ERROR :: Source Port is incorrect" + Style.RESET_ALL)
+                if rule["dport1"]:
+                    port = self.get_port(packet, ((ip_header) * 4) + 2)
+                    if port == None:
+                        print(Fore.YELLOW + "ERROR :: Source Port is incorrect" + Style.RESET_ALL)
+                        return None
+                    elif not self.is_port_in_range(port, rule["dport1"], rule["dport2"]):
+                        print(Fore.YELLOW + "FILTER :: Destination Port is not in specified range" + Style.RESET_ALL)
+                        return False
+
+            if (protocol == 1) and ((rule["protocol"] == "all") or (rule["protocol"] == "icmp")): # ICMP
+                type_field = self.get_icmp_packet(packet, (ip_header * 4))
+                if (type_field == None):
+                    print(Fore.YELLOW + "ERROR :: ICMP Type is incorrect" + Style.RESET_ALL)
                     return None
-                elif not self.is_port_in_range(port, rule["dport1"], rule["dport2"]):
-                    print(Fore.YELLOW + "FILTER :: Destination Port is not in specified range" + Style.RESET_ALL)
+
+            # Check for a potential port scan 
+            dport = self.get_port(packet, ((ip_header) * 4) + 2)
+            if rule["action"] == "ACCEPT" and not self.check_port_scan(socket.inet_ntoa(src_addr), dport, protocol):
+                return False
+            return True
+        if ver == 6:
+            srcip = ipaddr.IPv6Address(ipaddr.Bytes(packet[8:24]))
+            dstip = ipaddr.IPv6Address(ipaddr.Bytes(packet[24:40]))
+            
+            protocol = self.get_protocol_ipv6(packet)
+
+            #filter protocol
+            if (rule["protocol"] != "all") and (rule["protocol"] != protocol):
+                print(Fore.YELLOW + "FILTER :: Protocol not matched" + Style.RESET_ALL)
+                return False
+
+            #filter IP addresses
+            if rule["sourceip"] != "any":
+                if not self.is_IP_in_range(srcip, rule["sourceip"]):
+                    print(Fore.YELLOW + "FILTER :: IP not in specified range" + Style.RESET_ALL)
                     return False
+            #filter ports
+            if ((protocol == 6) or (protocol == 17)) and ((rule["protocol"] == "all") or (rule["protocol"] == "tcp") or (rule["protocol"] == "udp")):
+                if rule["sport1"]:
+                    port = self.get_port(packet, 40)
+                    if port == None:
+                        print(Fore.YELLOW + "ERROR :: Source Port is incorrect" + Style.RESET_ALL)
+                        return None
+                    elif not self.is_port_in_range(port, rule["sport1"], rule["sport2"]):
+                        print(Fore.YELLOW + "FILTER :: Source Port is not in specified range" + Style.RESET_ALL)
+                        return False
 
-        if (protocol == 1) and ((rule["protocol"] == "all") or (rule["protocol"] == "icmp")): # ICMP
-            type_field = self.get_icmp_packet(packet, (ip_header * 4))
-            if (type_field == None):
-                print(Fore.YELLOW + "ERROR :: ICMP Type is incorrect" + Style.RESET_ALL)
-                return None
+                if rule["dport1"]:
+                    port = self.get_port(packet, 42)
+                    if port == None:
+                        print(Fore.YELLOW + "ERROR :: Source Port is incorrect" + Style.RESET_ALL)
+                        return None
+                    elif not self.is_port_in_range(port, rule["dport1"], rule["dport2"]):
+                        print(Fore.YELLOW + "FILTER :: Destination Port is not in specified range" + Style.RESET_ALL)
+                        return False
+                        # Check for a potential port scan 
+            dport = self.get_port(packet, 42)
+            if rule["action"] == "ACCEPT" and not self.check_port_scan(srcip, dport, protocol):
+                return False
+            return True
+    
 
-        # Check for a potential port scan 
-        dport = self.get_port(packet, ((ip_header) * 4) + 2)    
-        if rule["action"] == "ACCEPT" and not self.check_port_scan(socket.inet_ntoa(src_addr), dport, protocol):
-            return False
-        return True
+
+
+
+
